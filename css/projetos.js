da670f6e64b6ba2adf135645fcc8e668207487e0
Gustavo Henrique Farias <84677653+Gustavolobosolitario@users.noreply.github.com>
2024-01-28 19:01:20 -0300

    Add files via upload

--- css/projetos.js
+++ css/projetos.js
@@ -1,14 +1,9 @@
-function gira_sombra(elemento){
-	const sombra = elemento.querySelector('.card-project-shadow')
-	console.log(sombra)
-	sombra.style.transition = "0.4s"
-	sombra.style.transform = "rotate(-30deg)"
-}
+function gira_sombra(elemento) {
+    const sombra = elemento.querySelector('.card-project-shadow');
+    sombra.style.transform = "rotate(-30deg)";
+  }
 
-
-function desgira_sombra(elemento){
-	const sombra = elemento.querySelector('.card-project-shadow')
-	sombra.style.transition = "0.4s"
-	sombra.style.transform = "rotate(0deg)"
-}
-
+  function desgira_sombra(elemento) {
+    const sombra = elemento.querySelector('.card-project-shadow');
+    sombra.style.transform = "rotate(0deg)";
+  }
